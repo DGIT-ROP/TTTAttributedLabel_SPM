f75a8978c2f5b73c62ab5aa063b715223b728f48
DGIT-ROP <dgit.rop@gmail.com>
2024-08-08 23:56:43 +0400

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .target(
             name: "TTTAttributedLabel",
             dependencies: [],
-            exclude: ["TTTAttributedLabel.h", "TTTAttributedLabel.m"],)
+            path: "Sources")
     ]
 )
